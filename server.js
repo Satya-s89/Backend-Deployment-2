4858304d5904005aed9799cc50705cf3702385e3
Satya-s89 <kaki.harisatya.s89@kalvium.community>
2025-03-06 15:39:14 +0530

    y

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ app.get('/hello', (req, res) => {
   res.send('Hello world this is my first backend deployment');
 });
 
+app.get('/app-info/:id', (req, res) => {
+  try{
+    console.log(req.params);
+    res.send("sucessfull");
+  }catch(error){
+    res.status(500).json({error: error.message});
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
